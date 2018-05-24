b4e5d6814a538ae6176831ae98b00e148beb111b
egortsaryk9 <egortsaryk@gmail.com>
2018-05-24 06:29:40 +0300

    Using http transport for connection to deipd node

--- config/environment/development.js
+++ config/environment/development.js
@@ -5,7 +5,7 @@ module.exports = {
         }
     },
     blockchain: {
-        rpcEndpoint: "ws://206.189.175.10",
+        rpcEndpoint: "http://206.189.175.10",
         chainId: "b49d215ad6bbf15da499e68c5669e6fa5ff424c954e1dff5488cb114801f32c0"
     },
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
