b547c366c9abdde1a31680aaa0a4886eca333c21
egortsaryk9 <egortsaryk@gmail.com>
2018-08-06 01:50:20 +0300

    chain id

--- config/environment/production.js
+++ config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     blockchain: {
         rpcEndpoint: "http://206.189.175.10",
-        chainId: "7ff5fafc5211eeabe3ab20a3053336fb6df9c0d4df994ebd6b8dc6b2fa1f1caf"
+        chainId: "76e1917c1c4fc93966f80d5f1484b64e5e89c759adbf52b402b764332bcade1c"
     },
     host: 'http://146.185.131.175',
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
