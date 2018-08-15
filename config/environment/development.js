f5fba8903aaa9d440c9d41cb837fbb0ef7528b6a
egormarushko <nebegan@gmail.com>
2018-08-15 22:56:45 +0300

    Update config

--- config/environment/development.js
+++ config/environment/development.js
@@ -5,7 +5,7 @@ module.exports = {
         }
     },
     blockchain: {
-        rpcEndpoint: "http://206.189.175.10",
+        rpcEndpoint: "https://dev-full-node.deip.world/",
         chainId: "9c90084a1527c5ca3e111213ff27a8e79fc7488052b5ce8d73a9a5cc2164081f"
     },
     host: 'http://localhost:8081',
--- config/environment/production.js
+++ config/environment/production.js
@@ -5,10 +5,10 @@ module.exports = {
         }
     },
     blockchain: {
-        rpcEndpoint: "http://206.189.175.10",
+        rpcEndpoint: "https://dev-full-node.deip.world/",
         chainId: "9c90084a1527c5ca3e111213ff27a8e79fc7488052b5ce8d73a9a5cc2164081f"
     },
-    host: 'http://146.185.131.175',
+    host: 'https://dev-server.deip.world/',
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
     jwtSecret: 'shhhhhhhhhhh!!!'
 };
