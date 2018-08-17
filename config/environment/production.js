035f0581739d3f286934a7af3cff5a07ebd6d0ff
egormarushko <nebegan@gmail.com>
2018-08-17 20:15:09 +0300

    Fix config

--- config/environment/production.js
+++ config/environment/production.js
@@ -5,10 +5,10 @@ module.exports = {
         }
     },
     blockchain: {
-        rpcEndpoint: "https://dev-full-node.deip.world/",
+        rpcEndpoint: "https://dev-full-node.deip.world",
         chainId: "9c90084a1527c5ca3e111213ff27a8e79fc7488052b5ce8d73a9a5cc2164081f"
     },
-    host: 'https://dev-server.deip.world/',
+    host: 'https://dev-server.deip.world',
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
     jwtSecret: 'shhhhhhhhhhh!!!'
 };
