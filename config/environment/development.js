93fb15c71012ad0c5554293699d3d0153b564afa
egortsaryk9 <egortsaryk@gmail.com>
2018-10-18 20:20:04 +0300

    chain_id is upated

--- config/environment/development.js
+++ config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     blockchain: {
         rpcEndpoint: "http://127.0.0.1:8090",
-        chainId: "c086d6b8057cf60b01f5a77626ed4412ce960f48d71c1a43d4e6f525f0e01670"
+        chainId: "9c913028fef5d756112d2788b7eb019d0644ed3cb61ff113f393ee96b3d0a25f"
     },
     host: 'http://127.0.0.1:8081',
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
--- config/environment/production.js
+++ config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     blockchain: {
         rpcEndpoint: "https://dev-full-node.deip.world",
-        chainId: "51469c2613aecd065d28822862fa1bae2fe81bc029df884f98ccef6bbe36fea8"
+        chainId: "9c913028fef5d756112d2788b7eb019d0644ed3cb61ff113f393ee96b3d0a25f"
     },
     host: 'https://dev-server.deip.world',
     sigSeed: "quickbrownfoxjumpsoverthelazydog",
