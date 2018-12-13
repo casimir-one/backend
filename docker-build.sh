bc7a60ac7850d046cf2eece99b5f858d71b6ab53
egortsaryk9 <egortsaryk@gmail.com>
2018-12-13 23:57:53 +0300

    web-server Docker image is renamed

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,9 @@ set -e
 TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
 
-echo "Building deipworld/server image..."
-export IMAGE_NAME="deipworld/server:$TAG"
-export LATEST_IMAGE_NAME="deipworld/server:$LATEST"
+echo "Building deipworld/web-server image..."
+export IMAGE_NAME="deipworld/web-server:$TAG"
+export LATEST_IMAGE_NAME="deipworld/web-server:$LATEST"
 export NODE_ENV="development"
 docker build -t=${IMAGE_NAME} .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
