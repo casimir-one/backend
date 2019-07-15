4450cb072dfa21067f30c36f63701f57d907c3c6
egortsaryk9 <egortsaryk@gmail.com>
2019-07-15 13:38:10 +0300

    dev-ops files

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,9 @@ set -e
 TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
 
-echo "Building deipworld/web-server image..."
-export IMAGE_NAME="deipworld/web-server:$TAG"
-export LATEST_IMAGE_NAME="deipworld/web-server:$LATEST"
+echo "Building deipworld/ip-protection-platform-web-server image..."
+export IMAGE_NAME="deipworld/ip-protection-platform-web-server:$TAG"
+export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-web-server:$LATEST"
 export NODE_ENV="development"
 docker build -t=${IMAGE_NAME} .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
