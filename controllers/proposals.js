c95fb0528102b72909804a38c823e9082a2d58d1
egortsaryk9 <egortsaryk@gmail.com>
2019-07-18 22:43:45 +0300

    Server clean up

--- controllers/proposals.js
+++ controllers/proposals.js
@@ -1,86 +1,7 @@
 import { authorizeResearchGroup } from './../services/auth'
-import { findResearchContentByHash, lookupContentProposal, proposalIsNotExpired } from './../services/researchContent'
 import { sendProposalNotificationToGroup } from './../services/notifications';
 import { sendTransaction, getTransaction } from './../utils/blockchain';
 import deipRpc from '@deip/deip-rpc-client';
-
-const createContentProposal = async (ctx) => {
-    const jwtUsername = ctx.state.user.username;
-    const tx = ctx.request.body;
-    const type = ctx.params.type;
-
-    const operation = tx['operations'][0];
-    const payload = operation[1];
-
-    const proposal = JSON.parse(payload.data);
-    const hash = type === 'dar' 
-        ? proposal.content.slice(4) : type === 'file' 
-        ? proposal.content.slice(5) : type === 'package' 
-        ? proposal.content.slice(8) : null;
-
-    const researchId = proposal.research_id;
-    const opGroupId = parseInt(payload.research_group_id);
-
-    if (!hash || isNaN(opGroupId)) {
-        ctx.status = 400;
-        ctx.body = `Mallformed operation: "${operation}"`;
-        return;
-    }
-
-    try {
-        const authorized = await authorizeResearchGroup(opGroupId, jwtUsername);
-        if (!authorized) {
-            ctx.status = 401;
-            ctx.body = `"${jwtUsername}" is not a member of "${opGroupId}" group`
-            return;
-        }
-
-        /* proposal specific action code */
-        const rc = await findResearchContentByHash(researchId, hash);
-        if (!rc) {
-            ctx.status = 404;
-            ctx.body = `Research content with hash "${hash}" does not exist`
-            return;
-        }
-        if (rc.status != 'in-progress') {
-            ctx.status = 405;
-            ctx.body = `Research content "${rc.title}" has '${rc.status}' status`
-            return;
-        }
-
-        const existingProposal = await lookupContentProposal(opGroupId, hash, type)
-        if (existingProposal && proposalIsNotExpired(existingProposal)) {
-            ctx.status = 409;
-            ctx.body = `Proposal for content with hash '${hash}' already exists: ${existingProposal}`
-            return;
-        }
-
-        rc.status = 'proposed';
-        rc.authors = proposal.authors;
-        rc.references = proposal.references;
-        rc.title = proposal.title;
-        const updatedRc = await rc.save();
-        const result = await sendTransaction(tx);
-        if (result.isSuccess) {
-            await processNewProposal(payload, result.txInfo);
-            ctx.status = 200;
-            ctx.body = updatedRc;
-        } else {
-            throw new Error(`Could not proceed the transaction: ${tx}`);
-        }
-    } catch(err) {
-        console.log(err);
-        const rollback = async (hash) => {
-            const rc = await findResearchContentByHash(researchId, hash);
-            rc.status = 'in-progress';
-            await rc.save();
-        }
-
-        await rollback(hash);
-        ctx.status = 500;
-        ctx.body = err.message;
-    }
-}
 
 const createResearchProposal = async (ctx) => {
     const jwtUsername = ctx.state.user.username;
@@ -184,6 +105,5 @@ async function processNewProposal(payload, txInfo) {
 
 export default {
     createResearchProposal,
-    createContentProposal,
     createInviteProposal
 }
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,9 @@
 import koa_router from 'koa-router'
 import users from '../controllers/users'
-import search from '../controllers/search'
 import notifications from '../controllers/notifications'
 import proposals from '../controllers/proposals'
 import groups from '../controllers/groups'
 import invites from '../controllers/invites'
-import agency from '../controllers/agency'
 import files from '../controllers/files'
 
 const router = koa_router()
@@ -17,14 +15,11 @@ router.get('/user/profiles', users.getUsersProfiles)
 router.post('/user/profile/:username', users.createUserProfile)
 router.put('/user/profile/:username', users.updateUserProfile)
 
-router.get('/search/contents/all', search.getAllResearchContents)
-
 router.get('/notifications/user/:username', notifications.getNotificationsByUser)
 router.put('/notifications/:username/mark-read/:notificationId', notifications.markUserNotificationAsRead)
 router.put('/notifications/:username/mark-all-read', notifications.markAllUserNotificationAsRead)
 
 router.post('/proposals/research', proposals.createResearchProposal)
-router.post('/proposals/content/:type', proposals.createContentProposal)
 router.post('/proposals/invite', proposals.createInviteProposal)
 
 router.post('/groups', groups.createResearchGroup)
@@ -33,9 +28,6 @@ router.put('/groups/profile/:permlink', groups.updateGroupProfile)
 
 router.post('/invites/approve', invites.approveInvite)
 router.post('/invites/reject', invites.rejectInvite)
-
-router.get('/agencies/profile/:agency', agency.getAgencyProfile)
-router.get('/agencies/profiles', agency.getAgenciesProfiles)
 
 router.get('/refs/project/:projectId', files.listFileRefs)
 router.get('/refs/project/file-id/:refId', files.getFileRefById)
