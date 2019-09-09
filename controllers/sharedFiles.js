6c5a76c42faa53896fd07dc3fb879dfc54e46117
Dmitry Nechay <dmitry.nechay@deip.world>
2019-09-09 11:55:44 +0300

    Add permission checks to shared files resource

--- controllers/sharedFiles.js
+++ controllers/sharedFiles.js
@@ -5,9 +5,18 @@ import filesService from './../services/fileRef';
 const getSharedFile = async (ctx) => {
   const sharedFileId = ctx.params.id;
   const isDownload = ctx.query.download === 'true';
+  const jwtUsername = ctx.state.user.username;
 
   try {
     const sharedFile = await sharedFilesService.getSharedFileById(sharedFileId);
+    if (!sharedFile) {
+      ctx.status = 404;
+      return;
+    }
+    if (![sharedFile.sender, sharedFile.receiver].includes(jwtUsername)) {
+      ctx.status = 403;
+      return;
+    }
     if (isDownload) {
       const fileRef = await filesService.findFileRefById(sharedFile.fileRefId);
       ctx.response.set('Content-disposition', `attachment; filename="${fileRef.filename}.aes"`);
@@ -38,10 +47,18 @@ const getSharedFiles = async (ctx) => {
 };
 
 const askPermission = async (ctx) => {
+  const sharedFileId = ctx.params.id;
+  const jwtUsername = ctx.state.user.username;
+
   try {
+    const sharedFile = await sharedFilesService.getSharedFileById(sharedFileId);
+    if (sharedFile.receiver !== jwtUsername) {
+      ctx.status = 403;
+      return;
+    }
     console.log(JSON.stringify(ctx.request.body, null, 2));
     // sign tx; if success - go next
-    const updatedSharedFile = await sharedFilesService.askPermissionToSharedFile(ctx.params.id);
+    const updatedSharedFile = await sharedFilesService.askPermissionToSharedFile(sharedFileId);
     ctx.status = 200;
     ctx.body = updatedSharedFile;
   } catch (err) {
@@ -53,14 +70,20 @@ const askPermission = async (ctx) => {
 
 const unlockFile = async (ctx) => {
   const sharedFileId = ctx.params.id;
+  const jwtUsername = ctx.state.user.username;
+
   try {
     const {
       signedTx,
       accessKey,
     } = ctx.request.body;
-    console.log(JSON.stringify(ctx.request.body, null, 2));
+    console.log(JSON.stringify(signedTx, null, 2));
     // sign tx; if success - go next
     const sharedFile = await sharedFilesService.getSharedFileById(sharedFileId);
+    if (sharedFile.sender !== jwtUsername) {
+      ctx.status = 403;
+      return;
+    }
     await filesService.addAccessKeyToFileRef(sharedFile.fileRefId, accessKey);
     const updatedSharedFile = await sharedFilesService.unlockSharedFile(sharedFileId);
     ctx.status = 200;
