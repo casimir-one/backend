2fb1cb26bbcc6e1ddc2b9bf25406302350129874
egortsaryk9 <egortsaryk@gmail.com>
2019-11-10 18:24:09 +0300

    spb-demo-build.sh

--- spb-demo-build.sh
+++ spb-demo-build.sh
@@ -5,8 +5,8 @@ TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
 
 echo "Building deipworld/web-server image..."
-export IMAGE_NAME="deipworld/web-server:$TAG"
-export LATEST_IMAGE_NAME="deipworld/web-server:$LATEST"
+export IMAGE_NAME="deipworld/spb-demo-web-server:$TAG"
+export LATEST_IMAGE_NAME="deipworld/spb-demo-web-server:$LATEST"
 export NODE_ENV="development"
 docker build -t=${IMAGE_NAME} .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
