d5b9306cf80a16da0fa7873fbb8a80bb5583bc09
egortsaryk9 <egortsaryk@gmail.com>
2019-11-26 21:41:09 +0300

    dev-ops script

--- spb-demo-build.sh
+++ spb-demo-build.sh
@@ -3,12 +3,16 @@ set -e
 
 TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
+ORG="deipworld"
+IMAGE_PREFIX="spb-demo"
 
-echo "Building deipworld/spb-demo-web-server image..."
-export IMAGE_NAME="deipworld/spb-demo-web-server:$TAG"
-export LATEST_IMAGE_NAME="deipworld/spb-demo-web-server:$LATEST"
+echo "Building $ORG/$IMAGE_PREFIX-web-server image..."
+export IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-server:$TAG"
+export LATEST_IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-server:$LATEST"
 export NODE_ENV="development"
 docker build -t=${IMAGE_NAME} .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
+docker rmi "$ORG/$IMAGE_PREFIX-$NODE_PREFIX:$TAG"
+docker rmi "$ORG/$IMAGE_PREFIX-$NODE_PREFIX:$LATEST"
