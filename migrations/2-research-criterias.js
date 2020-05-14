cc8fde7bca3b52d0b425175bad6137ef62f5d80c
egortsaryk9 <egortsaryk@gmail.com>
2020-05-14 18:52:28 +0300

    Research criterias migration

--- migrations/2-research-criterias.js
+++ migrations/2-research-criterias.js
@@ -20,10 +20,29 @@ deipRpc.api.setOptions({ url: config.blockchain.rpcEndpoint });
 deipRpc.config.set('chain_id', config.blockchain.chainId);
 mongoose.connect(config.mongo['deip-server'].connection);
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const run = async () => {
-  const trlIndex = Math.floor(Math.random() * 7) + 1  
-  await Research.update({}, { $set: { "tenantCriterias": [{ "component": "5ebd469a2cea71001f84345a", "type": RESEARCH_COMPONENT_TYPE.STEPPER, "value": { index: trlIndex} }] } }, { multi: true });
+
+  await Research.update({}, { $set: { "tenantCriterias": [] } }, { multi: true });
   await Research.update({}, { $unset: { "trl": "" } }, { multi: true });
+
+  const researches = await Research.find({});
+  let promises = [];
+
+  for (let i = 0; i < researches.length; i++) {
+    let research = researches[i];
+    const trlIndex = getRandomInt(0, 7);
+    research.tenantCriterias = [{ "component": "5ebd469a2cea71001f84345a", "type": RESEARCH_COMPONENT_TYPE.STEPPER, "value": { index: trlIndex } }];
+    promises.push(research.save());
+  }
+
+  return Promise.all(promises);
+
 };
 
 run()
