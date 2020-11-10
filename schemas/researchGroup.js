5ceeca6d4071ba91f6ca6b9a2520a8a7bc806e52
egortsaryk9 <egortsaryk@gmail.com>
2020-11-10 17:53:36 +0200

    Research groups

--- schemas/researchGroup.js
+++ schemas/researchGroup.js
@@ -3,18 +3,11 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const ResearchArea = new Schema({
-  "_id": false,
-  "id": { type: String, required: true },
-  "parentId": { type: String },
-  "title": { type: String, required: true },
-  "disciplines": [{ type: String, required: true }],
-});
 
 const ResearchGroup = new Schema({
   "_id": { type: String, required: true },
   "creator": { type: String, required: true },
-  "researchAreas": [ResearchArea],
+  "researchAreas": [Object],
 
 }, { timestamps: { createdAt: 'created_at', 'updatedAt': 'updated_at' } });
 
