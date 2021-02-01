2f22ad21ecdabcf5a7c1707f5fea048a030c5520
egortsaryk9 <egortsaryk@gmail.com>
2021-02-02 00:03:20 +0100

    debugging info deleted

--- middlewares/tenantResearchContentAccess.js
+++ middlewares/tenantResearchContentAccess.js
@@ -1,11 +1,9 @@
-import request from 'request';
 import ResearchService from './../services/research';
 import TenantService from './../services/tenant';
 import util from 'util';
 
 
 async function tenantResearchContentAccess(ctx, next) {
-  // ctx.assert(ctx.state.isTenantAdmin, 401);
   const researchService = new ResearchService();
   const tenantService = new TenantService();
   const researchExternalId = ctx.params.researchExternalId;
@@ -14,11 +12,9 @@ async function tenantResearchContentAccess(ctx, next) {
   ctx.assert(!!research, 404);
 
   if (research.tenantId == ctx.state.tenant._id) {
-    let i = 5;
     ctx.state.requestedTenant = ctx.state.tenant;
     await next();
   } else {
-    let i = 5;
     const researchTenant = await tenantService.getTenant(research.tenantId);
 
     const tenantAccount = researchTenant.account;
