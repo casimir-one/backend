38f8eedccc3c7ae1e0fe9459488771a9e3ca8d99
egortsaryk9 <egortsaryk@gmail.com>
2021-04-07 17:30:10 +0200

    Review assessment criterias

--- migrations/6-research-attribute-areas.js
+++ migrations/6-research-attribute-areas.js
@@ -14,8 +14,6 @@ const bluebird = require('bluebird');
 const TenantProfile = require('./../schemas/tenant');
 
 const deipRpc = require('@deip/rpc-client');
-const RESEARCH_ATTRIBUTE_TYPE = require('./../constants/researchAttributeTypes').default;
-const RESEARCH_ATTRIBUTE_AREA = require('./../constants/researchAttributeArea').default;
 
 
 deipRpc.api.setOptions({ url: config.DEIP_FULL_NODE_URL });
@@ -24,32 +22,7 @@ mongoose.connect(config.DEIP_MONGO_STORAGE_CONNECTION_URL);
 
 
 const run = async () => {
-  const MULTI_SELECT = "multi-select";
 
-  const tenantPromises = [];
-  const tenants = await TenantProfile.find({});
-
-  for (let i = 0; i < tenants.length; i++) {
-    let tenantProfile = tenants[i];
-
-    for (let j = 0; j < tenantProfile.settings.researchAttributes.length; j++) {
-      let attr = tenantProfile.settings.researchAttributes[j];
-
-      if (attr.type == RESEARCH_ATTRIBUTE_TYPE.STEPPER) {
-        attr.areas = [RESEARCH_ATTRIBUTE_AREA.SIDEBAR, RESEARCH_ATTRIBUTE_AREA.CARD];
-      } else if (attr.type == RESEARCH_ATTRIBUTE_TYPE.PARTNERS || attr.type == RESEARCH_ATTRIBUTE_TYPE.SELECT || attr.type == MULTI_SELECT) {
-        attr.areas = [RESEARCH_ATTRIBUTE_AREA.SIDEBAR];
-      } else {
-        attr.areas = [RESEARCH_ATTRIBUTE_AREA.MAIN];
-      }
-
-      attr.order = j + 1;
-    }
-
-    tenantPromises.push(tenantProfile.save());
-  }
-  
-  await Promise.all(tenantPromises);
 };
 
 run()
