2564c2c6bfa4df1e1affcdabf2b5bf6cadbe2b95
egortsaryk9 <egortsaryk@gmail.com>
2021-05-06 04:08:24 +0200

    Cron polling job disabled

--- src/queue/index.js
+++ src/queue/index.js
@@ -8,21 +8,22 @@ import { QUEUE_TOPIC } from './../constants';
 var isBroadcasting = false;
 const eventsBuffer = [];
 
-PubSub.subscribe(QUEUE_TOPIC.APP_EVENT_TOPIC, function (topic, events) {
-  eventsBuffer.push(...events);
+PubSub.subscribe(QUEUE_TOPIC.APP_EVENT_TOPIC, async function (topic, events) {
+  // eventsBuffer.push(...events);
+  await BaseEventHandler.Broadcast(events);
 });
 
-cron.schedule('*/0.5 * * * * *', async () => {
+// cron.schedule('*/0.5 * * * * *', async () => {
 
-  if (!isBroadcasting) {
-    isBroadcasting = true;
-    const eventsBatch = eventsBuffer.splice(0, eventsBuffer.length)
-    try { 
-      await BaseEventHandler.Broadcast(eventsBatch);
-    } catch (err) {
-      console.error("Broadcast error: ", err);
-    }
-    isBroadcasting = false;
-  }
+//   if (!isBroadcasting) {
+//     isBroadcasting = true;
+//     const eventsBatch = eventsBuffer.splice(0, eventsBuffer.length)
+//     try { 
+//       await BaseEventHandler.Broadcast(eventsBatch);
+//     } catch (err) {
+//       console.error("Broadcast error: ", err);
+//     }
+//     isBroadcasting = false;
+//   }
   
-});
+// });
