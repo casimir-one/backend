7644af603df0602330c0edba486afd7b94912b71
Egor Anikey <egor.anikey@deip.world>
2021-08-16 17:46:44 +0300

    Fix: change document body type to object

--- src/schemas/DocumentTemplateSchema.js
+++ src/schemas/DocumentTemplateSchema.js
@@ -5,10 +5,10 @@ const Schema = mongoose.Schema;
 const DocumentTemplateSchema = new Schema({
   "account": { type: String, required: true },
   "title": { type: String, required: false },
-  "body": { type: String, required: true },
+  "body": { type: Object, required: true },
   "creator": { type: String, required: true },
   "tenantId": { type: String, required: true },
-});
+}, { timestamps: { createdAt: true, updatedAt: true } });
 
 const model = mongoose.model('document-template', DocumentTemplateSchema);
 
