d834b603ad83bcb5806ee473b97016881bfb0c2d
Stas <dashchinskiystas@gmail.com>
2022-07-01 13:59:26 +0200

    chore: route renamed from api/v2/user/exists/username-or-email/:usernameOrEmail to auth/v2/exist/:usernameOrEmail

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,6 +7,7 @@ const public_route = koa_router()
 public_route.post('/sign-in', auth.signIn)
 public_route.post('/v2/sign-up', usersCtrl.createUser)
 public_route.post('/v2/import-dao', usersCtrl.importDAO)
+public_route.get('/v2/exist/:usernameOrEmail', usersCtrl.checkIfUserExists)
 
 const routes = {
   public: koa_router().use('/auth', public_route.routes())
